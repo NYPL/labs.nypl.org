8f0461c4857505356ee8f5aeb1809d2dfa793091
David Riordan <david.riordan@gmail.com>
2014-03-06 19:21:03 -0500

    trying something new

--- deploy_listener.js
+++ deploy_listener.js
@@ -1,2 +1,2 @@
 var hookshot = require('hookshot');
-hookshot('refs/heads/master', 'git fetch origin && git checkout origin/master && npm rebuild && restart ghost').listen(1911);
+hookshot('refs/heads/master', 'stop ghost && git fetch origin && git checkout origin/master && npm rebuild && start ghost').listen(1911);
